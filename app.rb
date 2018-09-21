17f827396b71aa7722270813f020134f160d0f74
Will Carhart <wcarhart@sandiego.edu>
2018-09-20 20:31:12 -0700

    Testing repeat

--- app.rb
+++ app.rb
@@ -39,8 +39,12 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    @game.guess(letter)
-    redirect '/show'
+    status = @game.guess(letter)
+    if !status
+      flash[:message] = "You already guessed #{letter}"
+    else
+      redirect '/show'
+    end
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -49,7 +53,6 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
     state = @game.check_win_or_lose
     if state == :play
       erb :show # You may change/remove this line
